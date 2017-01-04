d0b8e8b545097a9013a150f9c9b4bd6af6d23b15
dan <dan.ohare@hssnet.com>
2017-01-04 11:17:21 +0000

    Added some commented out logging for debugging purposes

--- src/test/CrossbowSpellCheckProviderSpec.js
+++ src/test/CrossbowSpellCheckProviderSpec.js
@@ -3,26 +3,31 @@ var CrossbowSpellCheckProvider = require('../index.js');
 var spellchecker = new CrossbowSpellCheckProvider();
 var wordlist = spellchecker.getWordList();
 
-describe('CrossbowSpellCheckerProvider: ', function() {   
-    
-    it('should be able to check words in wordlist are suggested',
-        function() {
-	
-	invalidWords = [];
-	  for(i = 0; i < wordlist.length; i += 2) {
-	    var ukTerm = wordlist[i];
-	    var usEquiv = wordlist[i+1];
-	    if(ukTerm === usEquiv) {
-		continue;
-	    }
-	    
-	    assert.equal(true, spellchecker.correct(ukTerm));	       	    
-	    if(spellchecker.suggest(usEquiv).indexOf(ukTerm) == -1) {              	
-	      invalidWords.push(usEquiv);	
-	    }	        
-	  }	  
-	  assert.equal(0, invalidWords.length);
-	  
-    });   
-    
+describe('CrossbowSpellCheckerProvider: ', function() {
+
+	it('should be able to check words in wordlist are suggested', function() {
+		invalidWords = [];
+		for (i = 0; i < wordlist.length; i += 2) {
+			var ukTerm = wordlist[i];
+			var usEquiv = wordlist[i + 1];
+			if (ukTerm === usEquiv) {
+				continue;
+			}
+
+			assert.equal(true, spellchecker.correct(ukTerm));
+			if (spellchecker.suggest(usEquiv).indexOf(ukTerm) == -1) {
+				//console.log("usEquiv:" + usEquiv);
+				//console.log("suggestions:" + spellchecker.suggest(usEquiv));
+				invalidWords.push(usEquiv);
+			}
+		}
+		//console.log("invalid words:" + invalidWords);
+		assert.equal(0, invalidWords.length);
+
+	})
+
+	xit('shouldn\'t be marking storeys as correct', function() {
+		assert.equal(false, spellchecker.correct('storeys'));
+	})
+
 });
